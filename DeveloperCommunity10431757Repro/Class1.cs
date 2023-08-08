e8cff3f00efe73761eea096088a5a2db3d1e5063
Heiko Wobst <wobst.heiko1010@t-online.de>
2023-08-08 11:30:17 +0200

    initial commit
    repro for https://developercommunity.visualstudio.com/t/Code-analysis-CA1854-is-not-shown-in-Vis/10431757

---
+++ DeveloperCommunity10431757Repro/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace DeveloperCommunity10431757Repro
+{
+    public class Class1
+    {
+        private readonly Dictionary<string, string?> _dictionary = new();
+
+        public string? GetValue(string key)
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                return _dictionary[key];
+            }
+
+            return null;
+        }
+    }
+}
